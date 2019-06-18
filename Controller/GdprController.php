dcdb13a33b728257330b18a230b2815caad5e6c3
Masth0 <dev.mastho@gmail.com>
2019-06-18 17:56:00 +0200

    Set cookies

--- Controller/GdprController.php
+++ Controller/GdprController.php
@@ -5,6 +5,8 @@ namespace FluffyFactory\Bundle\GdprBundle\Controller;
 use FluffyFactory\Bundle\GdprBundle\Form\CookiesType;
 use FluffyFactory\Bundle\GdprBundle\Service\CookieService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -21,8 +23,17 @@ class GdprController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $response = new RedirectResponse($this->generateUrl('homepage')); // TODO mettre route redirigée dans les params du bundle yaml, et par défaut rediriger ici
 
-            return $this->redirectToRoute('homepage'); // TODO mettre route redirigée dans les params du bundle yaml, et par défaut rediriger ici
+            foreach ($data as $k => $value) {
+                $expireDate = new \DateTime();
+                $expireDate->modify('1 year');
+                $cookie = new Cookie($k, $value ? 1 : 0, $expireDate);
+                $response->headers->setCookie($cookie);
+            }
+
+            return $response;
         }
 
         return $this->render('@Gdpr/privacy.html.twig', [
