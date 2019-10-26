db630da4357d428032d542874829cdb164f0c795
Masth0 <dev.mastho@gmail.com>
2019-10-26 15:23:34 +0200

    Cookies wasn't added on edge, cause domain ...

--- Resources/public/js/cookies.js
+++ Resources/public/js/cookies.js
@@ -2,19 +2,17 @@
  * Add Cookie
  * @param {string} name
  * @param {string} value
- * @param {string} domain
  * @param {string} path
  * @param {boolean} secure
  */
-function setCookie(name, value, domain='', path ='/', secure) {
+export function setCookie(name, value, path ='/', secure) {
   var expireDate = new Date();
   expireDate.setMonth(expireDate.getMonth() + 12);
   var _nameValue = name + '=' + value + '; ';
   var _date = 'expires=' + expireDate.toUTCString() + '; ';
-  var _domain = 'domain=' + domain + '; ';
   var _path = 'path=' + path + '; ';
   var _secure = secure ? ' secure;' : '';
-  document.cookie = _nameValue + _date + _domain + _path + _secure;
+  document.cookie = _nameValue + _date + _path + _secure;
 }
 
 /**
